b70740d40bba55c0c39cfbe1b23af9e57ca0ad0b
gilmar42 <100540954+gilmar42@users.noreply.github.com>
2026-03-24 23:47:11 -0300

    fix: corrige configuracoes para producao e Node 22

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  experimental: {
+    turbopackHints: false,
+  },
   images: {
     remotePatterns: [
       {
